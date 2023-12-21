23c191c3c454a617e9ce5e2b81f29d7c599aa53e
renestavnes <renestavnes@hotmail.com>
2023-12-21 09:10:01 +0100

    '-'

--- packages/shared/typings.d.ts
+++ packages/shared/typings.d.ts
@@ -1,13 +1,15 @@
-import { Timestamp, FieldValue } from "firebase/firestore";
+//import { Timestamp, FieldValue } from "firebase/firestore";
 
 export interface Message {
-    timestamp: Timestamp | FieldValue;  //ATTENTION: possibly corrupted type    
+    //timestamp: Timestamp | FieldValue;  //ATTENTION: possibly corrupted type  
+    timestamp: FirebaseFirestore.Timestamp;  
     userId: string;
     content: string;
 }
 
 export interface Conversation {
-    timestamp: Timestamp | FieldValue; //ATTENTION: possibly corrupted type    
+    //timestamp: Timestamp | FieldValue; //ATTENTION: possibly corrupted type    
+    timestamp: FirebaseFirestore.Timestamp;
     userId: string;
     turnState: number;
     z: number;
