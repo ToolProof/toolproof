0f0981d2c9e2ee8465965e2a2d9627d51f7cf57e
renestavnes <renestavnes@hotmail.com>
2023-12-30 17:44:18 +0100

    '-'

--- packages/web-app/src/app/page.tsx
+++ packages/web-app/src/app/page.tsx
@@ -1,57 +1,51 @@
-"use client";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { signIn } from "next-auth/react";
 import { useEffect } from "react";
+import { signIn } from "next-auth/react";
 import { createConversationInFirestore } from "../lib/utils";
-
-
-import { useCollection } from "react-firebase-hooks/firestore";
-import { collection, query, where, orderBy } from "firebase/firestore";
+import { getDocs, query, where, collection, orderBy, limit } from "firebase/firestore";
 import { db } from "shared/firebaseClient";
-
 
 export default function Home() {
   const { data: session } = useSession();
   const router = useRouter();
-
-
   const userEmail = session?.user?.email;
-  const conversationsQuery = userEmail ? query(
-    collection(db, "conversations"),
-    where("userId", "==", userEmail),
-    orderBy("timestamp", "asc")
-  ) : null;
-  const [conversations, isLoading ] = useCollection(conversationsQuery);
-
 
   useEffect(() => {
-    const foo = async () => {
-      console.log("session: ", session);
-      console.log("isLoading: ", isLoading);
-      console.log("conversations: ", conversations);
-      if (session && !isLoading && (!conversations || conversations?.docs.length === 0)) {
-        const conversationId = await createConversationInFirestore(session, "base", 1);
-        if (conversationId) {
-          router.push(`/conversation/${conversationId}`);
+    const checkAndCreateConversation = async () => {
+      if (userEmail) {
+        const q = query(
+          collection(db, "conversations"),
+          where("userId", "==", userEmail),
+          orderBy("timestamp", "asc"),
+          limit(1)
+        );
+
+        const querySnapshot = await getDocs(q);
+        if (querySnapshot.empty) {
+          // Create conversation only if none exists
+          const conversationId = await createConversationInFirestore(session, "base", 1);
+          if (conversationId) {
+            router.push(`/conversation/${conversationId}`);
+          }
         }
       }
-    }
-    foo();
-  }, [session, router, isLoading, conversations]);
+    };
+
+    checkAndCreateConversation();
+  }, [userEmail, session, router]);
 
   return (
     <div className="baseBackground flex flex-col items-center justify-center h-screen">
       <div className="text-black text-5xl mb-4">toolproof.com</div>
       {
-        session?.user?.email === "renestavnes@hotmail.com" &&
+        userEmail === "renestavnes@hotmail.com" &&
         <div className="flex">
           {!session && (
             <button onClick={() => signIn("google")} className="text-black font-bold text-3xl animate-pulse">Sign In</button> //ATTENTION: project-number shown in google sign-in
           )
           }
         </div>
-
       }
     </div>
   );
