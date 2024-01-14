8b60a78afa99a3538d58272c86e8bd3a5c3d3bd9
renestavnes <renestavnes@hotmail.com>
2024-01-14 03:40:01 +0100

    -

--- packages/test/src/app/components/CO.tsx
+++ packages/test/src/app/components/CO.tsx
@@ -1,4 +1,4 @@
-import { collection } from "firebase/firestore";
+import { collection, query, orderBy } from "firebase/firestore";
 import { useCollection } from "react-firebase-hooks/firestore";
 import { db } from "shared/firebaseClient";
 import { MessageRead } from "shared/typings";
@@ -8,16 +8,21 @@ type Props = {
 }
 
 export default function Conversation({ conversationId }: Props) {
-    const messagesRef = collection(db, "conversations", conversationId, "messages");
-    console.log(messagesRef);
+    //const messagesRef = collection(db, "conversations", conversationId, "messages");
+    //console.log(messagesRef);
+    const [messageSnapshots, loading] = useCollection(query(
+            collection(db, "conversations", conversationId, "messages"),
+            orderBy("timestamp", "asc"),
+        )
+    );
+    const messages = messageSnapshots?.docs.map((doc) => doc.data() as MessageRead) ?? [];
     //const [messageSnapshots, loading] = useCollection(collection(db, "conversations", conversationId, "messages"));
     //const messages = messageSnapshots?.docs.map((doc) => doc.data() as MessageRead) ?? [];
 
     return (
         <div className="bg-white">
-            {/* {loading && <p>Loading...</p>}
-            {messages.length > 0 ? <p>{messages[0].content}</p> : <p>No messages</p>} */}
-            Hi, I'm Conversation
+            {loading && <p>Loading...</p>}
+            {messages &&  <p>{messages[0].content}</p>}
         </div>
     );
 
