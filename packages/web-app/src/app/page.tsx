6b3773a2e73970b744df5cb59f97892c1932736f
renestavnes <renestavnes@hotmail.com>
2024-01-17 06:15:46 +0100

    '-'

--- packages/web-app/src/app/page.tsx
+++ packages/web-app/src/app/page.tsx
@@ -1,24 +1,27 @@
 "use client"
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { signIn } from "next-auth/react";
 import { useConversations, addConversation } from "../lib/firestoreHelpersClient";
+
 
 export default function Home() {
   const { data: session } = useSession();
   const router = useRouter();
   const userEmail = session?.user?.email || "";
   const { conversations } = useConversations(userEmail);
+  const [isCreated, setIsCreated] = useState(false);
 
   useEffect(() => {
     const checkAndHandleConversation = async () => {
       if (userEmail) {
-        if (conversations.length === 0) {
+        if (conversations.length === 0 && !isCreated) { //ATTENTION: asynchronous updates of isCreated
           try {
             const result = await addConversation({ parentId: "base", userId: userEmail, turnState: 0 });
             if (result && result.data && result.data.conversationId) {
               router.push(`/conversation/${result.data.conversationId}`);
+              setIsCreated(true);
             } else {
               console.error("Conversation creation did not return a valid ID");
             }
@@ -28,13 +31,14 @@ export default function Home() {
         } else {
           // Redirect to the first conversation
           const existingConversationId = conversations[0].id;
+          setIsCreated(true);
           router.push(`/conversation/${existingConversationId}`);
         }
       }
     };
 
     checkAndHandleConversation();
-  }, [userEmail, conversations, router]);
+  }, [isCreated, userEmail, conversations, router]);
   
 
   return (
