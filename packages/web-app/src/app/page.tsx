e42080660301dd24d53e1b088d6c2c2e6c2a3e48
renestavnes <renestavnes@hotmail.com>
2024-02-03 12:51:23 +0100

    '-'

--- packages/web-app/src/app/page.tsx
+++ packages/web-app/src/app/page.tsx
@@ -6,12 +6,14 @@ export default function Home() {
   const { data: session } = useSession();
   const userEmail = session?.user?.email || "";  
 
+  const condition = userEmail === "renestavnes@hotmail.com"
+
 
   return (
     <div className="baseBackground flex flex-col items-center justify-center h-screen">
       <div className="text-black text-5xl mb-4">toolproof.com</div>
       {
-        userEmail === "renestavnes@hotmail.com" || true &&
+        condition &&
         <div className="flex">
           {!session && (
             <button onClick={() => signIn("google")} className="text-black font-bold text-3xl animate-pulse">Sign In</button> //ATTENTION: project-number shown in google sign-in
