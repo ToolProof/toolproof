ce77577c60ec0ea0523a99961f6b0e38839248ca
renestavnes <renestavnes@hotmail.com>
2024-02-06 12:21:02 +0100

    '-'

--- packages/web-app/src/app/layout.tsx
+++ packages/web-app/src/app/layout.tsx
@@ -3,7 +3,7 @@ import "@/setup/globals.css"
 import type { Metadata } from "next"
 import SideBar from "./components/SideBar"
 import { getServerSession } from "next-auth"
-import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+import { authOptions } from "@/setup/authOptions";
 import { SessionProvider } from "./components/SessionProvider"
 import ClientProvider from "./components/ClientProvider"
 import StoreProvider from "./components/StoreProvider"
@@ -23,7 +23,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body>
-        <SessionProvider session={session}> {/*ATTENTION: how come we have access to useContext on the server?*/}
+        <SessionProvider session={session}>
           <StoreProvider>
             <div className="flex">
               <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
