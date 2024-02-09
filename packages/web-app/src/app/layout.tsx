0198a9a98f64806d075b6aaa99fe1e1b1570e5c4
renestavnes <renestavnes@hotmail.com>
2024-02-09 11:49:39 +0100

    '-'

--- packages/web-app/src/app/layout.tsx
+++ packages/web-app/src/app/layout.tsx
@@ -19,7 +19,7 @@ export default async function RootLayout({
   children: React.ReactNode
 }) {
   const session = await getServerSession(authOptions)
-  //console.log("Session: ", session); // Debug line
+  
   return (
     <html lang="en">
       <body>
