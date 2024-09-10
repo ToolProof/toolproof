ca7788b972b7aace5e4deda2795dea23ff720341
renestavnes <renestavnes@hotmail.com>
2024-09-10 23:54:22 +0200

    -

--- packages/playroom/src/playpen/bootstrapping.ts
+++ packages/playroom/src/playpen/bootstrapping.ts
@@ -1,33 +1,13 @@
 
-interface __DataStructure<T extends __Operation> {
-    allowedOperations: T[];  // Array of allowed operations
+interface Concept {
+  name: string;
+  [key: string]: string;
 }
 
-interface __Array extends __DataStructure<__Insert | __Delete> {
-    // Arrays allow both Insert and Delete operations
-}
+const concept: Concept = {
+  name: 'exampleConcept',
+  key1: 'value1',
+  key2: 'value2',
+  key3: 'value2',
+};
 
-interface __Stack extends __DataStructure<__Push | __Pop> {
-    // Stacks only allow Push and Pop operations
-}
-
-interface __Operation {
-    type: string;  // A general type that all operations will extend
-  }
-  
-  interface __Insert extends __Operation {
-    type: 'Insert' | 'Push';  // Allowable types for Insert operations
-  }
-  
-  interface __Delete extends __Operation {
-    type: 'Delete' | 'Pop';  // Allowable types for Delete operations
-  }
-  
-  interface __Push extends __Insert {
-    type: 'Push';  // Specific to Push
-  }
-  
-  interface __Pop extends __Delete {
-    type: 'Pop';  // Specific to Pop
-  }
-  
