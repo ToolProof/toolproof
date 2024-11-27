ecdd7ea40d25be081c86337b8e924f42031d2f2c
renestavnes <renestavnes@hotmail.com>
2024-11-27 09:08:51 +0100

    -

---
+++ packages/playpen/src/concept/bootstrapping.ts
@@ -0,0 +1,13 @@
+
+interface Concept {
+  name: string;
+  [key: string]: string;
+}
+
+const concept: Concept = {
+  name: 'exampleConcept',
+  key1: 'value1',
+  key2: 'value2',
+  key3: 'value2',
+};
+
