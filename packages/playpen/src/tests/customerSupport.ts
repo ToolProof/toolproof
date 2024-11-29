3a138360def3b5ea219470b8e7a4c15d2186968e
renestavnes <renestavnes@hotmail.com>
2024-11-29 10:39:28 +0100

    -

--- packages/playpen/src/tests/customerSupport.ts
+++ packages/playpen/src/tests/customerSupport.ts
@@ -2,6 +2,7 @@ import { ChatTogetherAI } from "@langchain/community/chat_models/togetherai";
 import { Annotation, MessagesAnnotation } from "@langchain/langgraph";
 import { z } from "zod";
 import { zodToJsonSchema } from "zod-to-json-schema";
+import { StateGraph } from "@langchain/langgraph";
 
 const model = new ChatTogetherAI({
   model: "meta-llama/Meta-Llama-3.1-8B-Instruct-Turbo",
@@ -47,18 +48,173 @@ Otherwise, respond only with the word "RESPOND".`;
     role: "user",
     content: CATEGORIZATION_HUMAN_TEMPLATE,
   }],
-  {
-    response_format: {
-      type: "json_object",
-      schema: zodToJsonSchema(
-        z.object({
-          nextRepresentative: z.enum(["BILLING", "TECHNICAL", "RESPOND"]),
-        })
-      )
-    }
-  });
+    {
+      response_format: {
+        type: "json_object",
+        schema: zodToJsonSchema(
+          z.object({
+            nextRepresentative: z.enum(["BILLING", "TECHNICAL", "RESPOND"]),
+          })
+        )
+      }
+    });
   // Some chat models can return complex content, but Together will not
   const categorizationOutput = JSON.parse(categorizationResponse.content as string);
   // Will append the response message to the current interaction state
   return { messages: supportResponse, nextRepresentative: categorizationOutput.nextRepresentative };
 };
+
+const billingSupport = async (state: typeof StateAnnotation.State) => {
+  const SYSTEM_TEMPLATE =
+    `You are an expert billing support specialist for LangCorp, a company that sells computers.
+Help the user to the best of your ability, but be concise in your responses.
+You have the ability to authorize refunds, which you can do by transferring the user to another agent who will collect the required information.
+If you do, assume the other agent has all necessary information about the customer and their order.
+You do not need to ask the user for more information.
+
+Help the user to the best of your ability, but be concise in your responses.`;
+
+  let trimmedHistory = state.messages;
+  // Make the user's question the most recent message in the history.
+  // This helps small models stay focused.
+  if (trimmedHistory.at(-1)._getType() === "ai") {
+    trimmedHistory = trimmedHistory.slice(0, -1);
+  }
+
+  const billingRepResponse = await model.invoke([
+    {
+      role: "system",
+      content: SYSTEM_TEMPLATE,
+    },
+    ...trimmedHistory,
+  ]);
+  const CATEGORIZATION_SYSTEM_TEMPLATE =
+    `Your job is to detect whether a billing support representative wants to refund the user.`;
+  const CATEGORIZATION_HUMAN_TEMPLATE =
+    `The following text is a response from a customer support representative.
+Extract whether they want to refund the user or not.
+Respond with a JSON object containing a single key called "nextRepresentative" with one of the following values:
+
+If they want to refund the user, respond only with the word "REFUND".
+Otherwise, respond only with the word "RESPOND".
+
+Here is the text:
+
+
+${billingRepResponse.content}
+.`;
+  const categorizationResponse = await model.invoke([
+    {
+      role: "system",
+      content: CATEGORIZATION_SYSTEM_TEMPLATE,
+    },
+    {
+      role: "user",
+      content: CATEGORIZATION_HUMAN_TEMPLATE,
+    }
+  ], {
+    response_format: {
+      type: "json_object",
+      schema: zodToJsonSchema(
+        z.object({
+          nextRepresentative: z.enum(["REFUND", "RESPOND"]),
+        })
+      )
+    }
+  });
+  const categorizationOutput = JSON.parse(categorizationResponse.content as string);
+  return {
+    messages: billingRepResponse,
+    nextRepresentative: categorizationOutput.nextRepresentative,
+  };
+};
+
+const technicalSupport = async (state: typeof StateAnnotation.State) => {
+  const SYSTEM_TEMPLATE =
+    `You are an expert at diagnosing technical computer issues. You work for a company called LangCorp that sells computers.
+Help the user to the best of your ability, but be concise in your responses.`;
+
+  let trimmedHistory = state.messages;
+  // Make the user's question the most recent message in the history.
+  // This helps small models stay focused.
+  if (trimmedHistory.at(-1)._getType() === "ai") {
+    trimmedHistory = trimmedHistory.slice(0, -1);
+  }
+
+  const response = await model.invoke([
+    {
+      role: "system",
+      content: SYSTEM_TEMPLATE,
+    },
+    ...trimmedHistory,
+  ]);
+
+  return {
+    messages: response,
+  };
+};
+
+import { NodeInterrupt } from "@langchain/langgraph";
+
+const handleRefund = async (state: typeof StateAnnotation.State) => {
+  if (!state.refundAuthorized) {
+    console.log("--- HUMAN AUTHORIZATION REQUIRED FOR REFUND ---");
+    throw new NodeInterrupt("Human authorization required.")
+  }
+  return {
+    messages: {
+      role: "assistant",
+      content: "Refund processed!",
+    },
+  };
+};
+
+let builder = new StateGraph(StateAnnotation)
+  .addNode("initial_support", initialSupport)
+  .addNode("billing_support", billingSupport)
+  .addNode("technical_support", technicalSupport)
+  .addNode("handle_refund", handleRefund)
+  .addEdge("__start__", "initial_support");
+
+
+  builder = builder.addConditionalEdges("initial_support", async (state: typeof StateAnnotation.State) => {
+    if (state.nextRepresentative.includes("BILLING")) {
+      return "billing";
+    } else if (state.nextRepresentative.includes("TECHNICAL")) {
+      return "technical";
+    } else {
+      return "conversational";
+    }
+  }, {
+    billing: "billing_support",
+    technical: "technical_support",
+    conversational: "__end__",
+  });
+  
+  console.log("Added edges!");
+
+
+  builder = builder
+  .addEdge("technical_support", "__end__")
+  .addConditionalEdges("billing_support", async (state) => {
+    if (state.nextRepresentative.includes("REFUND")) {
+      return "refund";
+    } else {
+      return "__end__";
+    }
+  }, {
+    refund: "handle_refund",
+    __end__: "__end__",
+  })
+  .addEdge("handle_refund", "__end__");
+
+console.log("Added edges!");
+
+
+import { MemorySaver } from "@langchain/langgraph";
+
+const checkpointer = new MemorySaver();
+
+export const graph = builder.compile({
+  checkpointer,
+});
