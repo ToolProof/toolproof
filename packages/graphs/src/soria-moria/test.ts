34b49b5d4d481264a1cb94d4ab4429b3e6e7c3ce
renestavnes <renestavnes@hotmail.com>
2024-12-17 14:47:37 +0100

    -

--- packages/graphs/src/soria-moria/test.ts
+++ packages/graphs/src/soria-moria/test.ts
@@ -1,9 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import { Client } from "@langchain/langgraph-sdk";
 import { RemoteGraph } from "@langchain/langgraph/remote";
 
 
-const url = `https://swedish-pal-fd21683e4d295b9cbaa6b41412dfe328.default.us.langgraph.app`;
-const graphName = "swedishPal";
+const url = `https://spanish-pal-bf20aef1d10a52d1b8b04c76757c90f5.default.us.langgraph.app`;
+const graphName = "agent";
 const client = new Client({
     apiUrl: url,
 });
@@ -15,7 +18,7 @@ const thread = await client.threads.create();
 // invoke the graph with the thread config
 const config = { configurable: { thread_id: thread.thread_id }};
 const result = await remoteGraph.invoke({
-  messages: [{ role: "user", content: "Who was the youngest member of The Beatles?" }],
+  messages: [{ role: "user", content: "What is the difference between a kibbutz and a moshav?" }],
 }, config);
 
 // verify that the state was persisted to the thread
