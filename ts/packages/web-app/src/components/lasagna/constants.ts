0fff6fbc6c1fb09b67ab138de133c98194c15bb9
René Stavnes <renestavnes@hotmail.com>
2025-02-17 12:30:19 +0100

    -

--- ts/packages/web-app/src/components/lasagna/constants.ts
+++ ts/packages/web-app/src/components/lasagna/constants.ts
@@ -45,7 +45,7 @@ export const sequence: Array<[GraphElementNameType[], string]> = [
     [['Candidates', 'Candidates_Simulation', 'Simulation', 'Simulation_Results', 'Results'],
         'This stage involves specialized tools that support the drug discovery process through molecular docking, molecular dynamics, quantum mechanical free energy calculations, and more. These tools, often Python-based (e.g., AutoDock Vina, Schrödinger Suite), stress-test the Candidate’s ability to bind to target molecules, usually proteins.'],
     [['Results', 'Results_Agent', 'Agent', 'Agent_Papers', 'Papers'],
-        'Simulation results, available in various file formats depending on the Simulation tools used, are presented to the Agent for analysis. If the results are promising, the Agent decides to draft an academic Paper. A Paper documents the Agent’s reasoning, the Simulation process, and outcomes, offering suggestions for further action or future research.'],
+        'Simulation results, available in various file formats depending on the tools used, are presented to the Agent. If the results are promising, the Agent decides to draft an academic Paper to document its reasoning when crafting the Candidate, the Simulation process, and Results, offering suggestions for further action or future research.'],
     [['Papers', 'Papers_Human', 'Human'],
         'Humans can retrieve Papers via the web interface.'],
     [['Human', 'Human_Agent', 'Agent', 'Agent_Human'],
