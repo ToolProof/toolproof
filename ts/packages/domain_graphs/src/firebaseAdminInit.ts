02a1acfd60318114137097c02e4043fdc39f8aaf
René Stavnes <renestavnes@hotmail.com>
2025-05-14 23:12:37 +0200

    -

---
+++ ts/packages/domain_graphs/src/firebaseAdminInit.ts
@@ -0,0 +1,29 @@
+import { initializeApp, getApps, cert } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+import { Storage } from '@google-cloud/storage';
+
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n') || "",
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL
+};
+
+const app = getApps().length === 0
+  ? initializeApp({
+    credential: cert(serviceAccount),
+  })
+  : getApps()[0];
+
+const db = getFirestore(app);
+
+const storage = new Storage({
+  credentials: {
+    project_id: process.env.GCP_PROJECT_ID,
+    private_key: process.env.GCP_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    client_email: process.env.GCP_CLIENT_EMAIL,
+  }
+});
+
+const bucketName = 'tp_resources';
+
+export { db, storage, bucketName };
